98171d95591106404700b4541ed61ddd86a0838e
Andreas Nedbal <andy@pixelde.su>
2019-11-27 23:16:23 +0100

    Pingprovements: Add ability to change ping indicator text and sprite colors

--- Pingprovements/Pingprovements.cs
+++ Pingprovements/Pingprovements.cs
@@ -7,33 +7,40 @@ using UnityEngine;
 using System.Reflection;
 using UnityEngine.SceneManagement;
 using System.Linq;
+using System;
 
 namespace Pingprovements
 {
     [BepInDependency("com.bepis.r2api")]
-    [BepInPlugin("com.pixeldesu.pingprovements", "Pingprovements", "1.0.1")]
+    [BepInPlugin("com.pixeldesu.pingprovements", "Pingprovements", "1.1.0")]
     public class Pingprovements : BaseUnityPlugin
     {
-        /**
-         * <summary>
-         *      Config variable for the default ping lifetime
-         * </summary>
-         */
+        // Config variables for ping lifetimes
         public static ConfigWrapper<int> DefaultPingLifetime { get; set; }
-
-        /**
-         * <summary>
-         *      Config variable for the enemy ping lifetime
-         * </summary>
-         */
         public static ConfigWrapper<int> EnemyPingLifetime { get; set; }
-
-        /**
-         * <summary>
-         *      Config variable for the interactible ping lifetime
-         * </summary>
-         */
         public static ConfigWrapper<int> InteractiblePingLifetime { get; set; }
+
+
+        // Config variables for ping text colors
+        public static ConfigWrapper<string> DefaultPingColorConfig { get; set; }
+        public static ConfigWrapper<string> EnemyPingColorConfig { get; set; }
+        public static ConfigWrapper<string> InteractiblePingColorConfig { get; set; }
+
+
+        // Config variables for ping sprite colors
+        public static ConfigWrapper<string> DefaultPingSpriteColorConfig { get; set; }
+        public static ConfigWrapper<string> EnemyPingSpriteColorConfig { get; set; }
+        public static ConfigWrapper<string> InteractiblePingSpriteColorConfig { get; set; }
+
+
+        // Color instances
+        private Color DefaultPingColor;
+        private Color DefaultPingSpriteColor;
+        private Color EnemyPingColor;
+        private Color EnemyPingSpriteColor;
+        private Color InteractiblePingColor;
+        private Color InteractiblePingSpriteColor;
+
 
         /**
          * <summary>
@@ -46,25 +53,76 @@ namespace Pingprovements
         public void Awake()
         {
             DefaultPingLifetime = Config.Wrap<int>(
-                "Main",
+                "Durations",
                 "DefaultPingLifetime",
                 "Time in seconds how long a regular 'walk to' ping indicator should be shown on the map",
                 6
             );
 
             EnemyPingLifetime = Config.Wrap<int>(
-                "Main",
+                "Durations",
                 "EnemyPingLifetime",
                 "Time in seconds how long a ping indicator for enemies should be shown on the map",
                 8
             );
 
             InteractiblePingLifetime = Config.Wrap<int>(
-                "Main",
+                "Durations",
                 "InteractiblePingLifetime",
                 "Time in seconds how long a ping indicator for interactibles should be shown on the map",
                 30
             );
+
+            DefaultPingColorConfig = Config.Wrap<string>(
+                "Colors",
+                "DefaultPingColor",
+                "Color of the default ping, in UnityEngine.Color R/G/B/A Float format",
+                "0.525,0.961,0.486,1.000"
+            );
+
+            DefaultPingSpriteColorConfig = Config.Wrap<string>(
+                "SpriteColors",
+                "DefaultPingSpriteColor",
+                "Color of the default ping sprite, in UnityEngine.Color R/G/B/A Float format",
+                "0.527,0.962,0.486,1.000"
+            );
+
+            DefaultPingColor = ConvertStringToColor(DefaultPingColorConfig.Value);
+            DefaultPingSpriteColor = ConvertStringToColor(DefaultPingSpriteColorConfig.Value);
+
+            EnemyPingColorConfig = Config.Wrap<string>(
+                "Colors",
+                "EnemyPingColor",
+                "Color of the enemy ping, in UnityEngine.Color R/G/B/A Float format",
+                "0.820,0.122,0.122,1.000"
+            );
+
+            EnemyPingSpriteColorConfig = Config.Wrap<string>(
+                "SpriteColors",
+                "EnemyPingSpriteColor",
+                "Color of the enemy ping sprite, in UnityEngine.Color R/G/B/A Float format",
+                "0.821,0.120,0.120,1.000"
+            );
+
+            EnemyPingColor = ConvertStringToColor(EnemyPingColorConfig.Value);
+            EnemyPingSpriteColor = ConvertStringToColor(EnemyPingSpriteColorConfig.Value);
+
+            InteractiblePingColorConfig = Config.Wrap<string>(
+                "Colors",
+                "InteractiblePingColor",
+                "Color of the interactible ping, in UnityEngine.Color R/G/B/A Float format",
+                "0.886,0.871,0.173,1.000"
+            );
+
+            InteractiblePingSpriteColorConfig = Config.Wrap<string>(
+                "SpriteColors",
+                "InteractiblePingSpriteColor",
+                "Color of the interactible ping sprite, in UnityEngine.Color R/G/B/A Float format",
+                "0.887,0.870,0.172,1.000"
+            );
+
+            InteractiblePingColor = ConvertStringToColor(InteractiblePingColorConfig.Value);
+            InteractiblePingSpriteColor = ConvertStringToColor(InteractiblePingSpriteColorConfig.Value);
 
             On.RoR2.PingerController.SetCurrentPing += PingerController_SetCurrentPing;
 
@@ -99,24 +157,27 @@ namespace Pingprovements
             pingIndicator.pingOrigin = newPingInfo.origin;
             pingIndicator.pingNormal = newPingInfo.normal;
             pingIndicator.pingTarget = newPingInfo.targetGameObject;
+
             pingIndicator.RebuildPing();
+
+            SetPingIndicatorColor(ref pingIndicator);
 
             float fixedTimer = 0f;
 
-            int pingType = (int)pingIndicator
+            PingIndicator.PingType pingType = (PingIndicator.PingType) pingIndicator
                     .GetType()
                     .GetField("pingType", BindingFlags.NonPublic | BindingFlags.Instance)
                     .GetValue(pingIndicator);
 
             switch(pingType)
             {
-                case (int) PingIndicator.PingType.Default:
+                case PingIndicator.PingType.Default:
                     fixedTimer = (float) DefaultPingLifetime.Value;
                     break;
-                case (int) PingIndicator.PingType.Enemy:
+                case PingIndicator.PingType.Enemy:
                     fixedTimer = (float) EnemyPingLifetime.Value;
                     break;
-                case (int) PingIndicator.PingType.Interactable:
+                case PingIndicator.PingType.Interactable:
                     fixedTimer = (float) InteractiblePingLifetime.Value;
                     break;
             }
@@ -133,6 +194,45 @@ namespace Pingprovements
             {
                 self.CallCmdPing(self.currentPing);
             }
+        }
+
+        private void SetPingIndicatorColor(ref PingIndicator pingIndicator)
+        {
+            SpriteRenderer sprRenderer;
+            Color textColor = new Color(0,0,0,0);
+
+            PingIndicator.PingType pingType = (PingIndicator.PingType) pingIndicator
+                    .GetType()
+                    .GetField("pingType", BindingFlags.NonPublic | BindingFlags.Instance)
+                    .GetValue(pingIndicator);
+
+            switch(pingType)
+            {
+                case PingIndicator.PingType.Default:
+                    textColor = DefaultPingColor;
+                    sprRenderer = pingIndicator.defaultPingGameObjects[0].GetComponent<SpriteRenderer>();
+                    sprRenderer.color = DefaultPingSpriteColor;
+                    break;
+                case PingIndicator.PingType.Enemy:
+                    textColor = EnemyPingColor;
+                    sprRenderer = pingIndicator.enemyPingGameObjects[0].GetComponent<SpriteRenderer>();
+                    sprRenderer.color = EnemyPingSpriteColor;
+                    break;
+                case PingIndicator.PingType.Interactable:
+                    textColor = InteractiblePingColor;
+                    sprRenderer = pingIndicator.interactablePingGameObjects[0].GetComponent<SpriteRenderer>();
+                    sprRenderer.color = InteractiblePingSpriteColor;
+                    break;
+            }
+
+            pingIndicator.pingText.color = textColor;
+        }
+
+        private Color ConvertStringToColor(string colorString)
+        {
+            float[] colorValues = Array.ConvertAll(colorString.Split(','), float.Parse);
+
+            return new Color(colorValues[0], colorValues[1], colorValues[2], colorValues[3]);
         }
     }
 }
