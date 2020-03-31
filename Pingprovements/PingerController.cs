a7184b7880fcbc8219383289c99804755cd1e494
Andreas Nedbal <andy@pixelde.su>
2020-03-31 21:53:00 +0200

    Pingprovements: Adjust notification code to work with new Notifications

--- Pingprovements/PingerController.cs
+++ Pingprovements/PingerController.cs
@@ -262,18 +262,26 @@ namespace Pingprovements
                 if (localUser.userProfile.HasDiscoveredPickup(pickupIndex))
                 {
                     PickupDef pickup = PickupCatalog.GetPickupDef(pickupIndex);
+                    
+                    ItemDef item = ItemCatalog.GetItemDef(pickup.itemIndex);
+                    EquipmentDef equip = EquipmentCatalog.GetEquipmentDef(pickup.equipmentIndex);
+                    ArtifactDef artifact = ArtifactCatalog.GetArtifactDef(pickup.artifactIndex);
 
                     GenericNotification notification = Object
                         .Instantiate(Resources.Load<GameObject>("Prefabs/NotificationPanel2"))
                         .GetComponent<GenericNotification>();
 
-                    if (pickup.itemIndex != ItemIndex.None)
+                    if (item != null)
                     {
-                        notification.SetItem(pickup.itemIndex);
+                        notification.SetItem(item);
                     }
-                    else if (pickup.equipmentIndex != EquipmentIndex.None)
+                    else if (equip != null)
                     {
-                        notification.SetEquipment(pickup.equipmentIndex);
+                        notification.SetEquipment(equip);
+                    }
+                    else if (artifact != null)
+                    {
+                        notification.SetArtifact(artifact);
                     }
 
                     notification.transform.SetParent(RoR2Application.instance.mainCanvas.transform, false);
